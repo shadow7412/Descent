399fff05f24c3e556e89ce09a5b878bf122c5045
Shadow <huffy7412@gmail.com>
2012-04-01 15:50:04 +1000

    js.php will now fail gracefully - rather than stuffing all javascript when missing a file.

--- comprise/js.php
+++ comprise/js.php
@@ -4,7 +4,9 @@ ob_start("ob_gzhandler"); //Gzip page
 header("Content-Type: text/javascript");
 $file = array("jquery-1.7.2.min.js","jquery-ui-1.8.18.custom.min.js","jquery.dataTables.min.js","descent.js");
 foreach($file as $f){
-	fpassthru($h=fopen($f,"r"));
-	fclose($h);
+	if(file_exists($f)){
+		fpassthru($h=fopen($f,"r"));
+		fclose($h);
+	}
 }
 ?>
